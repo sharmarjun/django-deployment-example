459788fc9d88a00dbbe614f2b687dff2faefd74b
Arjun Sharma <rjun.sharma18@gmail.com>
2021-03-10 12:55:25 +0530

    first

---
+++ template_project/learning_templates/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from basic_app import views
+
+# TEMPLATE TAGGING
+app_name= 'basic_app'
+
+urlpatterns=[
+
+    url('^relative/$',views.relative,name='relative'),
+    url('^other/$',views.other,name='other'),
+
+]
